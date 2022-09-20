1429f619217cea7e51f01059705d3302902a5335
glory665 <glory665@mail.ru>
2022-09-21 00:13:55 +0300

    create GraphQL

--- library/settings.py
+++ library/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userapp',
+    'graphene_django',
 
     'rest_framework',
     'users',
@@ -163,4 +164,8 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+}
+
+GRAPHENE = {
+    "SCHEMA": "library.schema.schema"
 }
